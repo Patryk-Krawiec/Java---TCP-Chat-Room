b35553847f51eebb07210a3c18b3bb8aeed4fa5b
Patryk Krawiec <kontakt@krawiecpatryk.pl>
2022-09-23 13:49:25 +0200

    TCP connectionhandler and nickname improvement

--- src/Server.java
+++ src/Server.java
@@ -4,13 +4,18 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class Server implements Runnable {
+
+    private ArrayList<ConnectionHandler> connections;
 
     @Override
     public void run() {
         try (ServerSocket server = new ServerSocket(9999)) {
             Socket client = server.accept();
+            ConnectionHandler handler = new ConnectionHandler(client);
+            connections.add(handler);
         } catch (IOException e) {
             //TODO: handle
         }
@@ -20,7 +25,15 @@ public class Server implements Runnable {
         private Socket client;
         private BufferedReader in;
         private PrintWriter out;
+        private String nickname;
 
+        public void broadcast(String message) {
+            for (ConnectionHandler ch : connections) {
+                if (ch != null){
+                    ch.sendMessage(message);
+                }
+            }
+        }
         public ConnectionHandler(Socket client) {
             this.client = client;
         }
@@ -30,10 +43,34 @@ public class Server implements Runnable {
             try {
                 out = new PrintWriter(client.getOutputStream(), true);
                 in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-                out.println("Hello");
+                out.println("Enter nickname: ");
+                nickname = in.readLine();
+                System.out.println(nickname + " added!");
+                broadcast(nickname + " joined chat!");
+                String message;
+                while ((message = in.readLine()) != null) {
+                    if (message.startsWith("/nick ")) {
+                        String[] messageSplit = message.split(" ", 2);
+                        if (messageSplit.length == 2) {
+                            broadcast(nickname + " nickname has been changed to " + messageSplit[1]);
+                            System.out.println(nickname + " nickname has been changed to " + messageSplit[1]);
+                            nickname = messageSplit[1];
+                            out.println("Successfully changed nickname to: " + nickname);
+                        } else {
+                            out.println("No nickname provided");
+                        }
+                    } else if (message.startsWith("/quit")) {
+                        //TODO: quit
+                    } else {
+                        broadcast(nickname + ": " + message);
+                    }
+                }
             } catch (IOException e) {
                 //TODO: handle
             }
+        }
+        public void sendMessage(String message) {
+            out.println(message);
         }
     }
 }
