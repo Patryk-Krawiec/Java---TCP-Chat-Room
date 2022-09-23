4f61c5da88c2cd903fbd07842002698ae23612c1
Patryk Krawiec <kontakt@krawiecpatryk.pl>
2022-09-23 13:29:40 +0200

    Initial setup, implemented runnable, defined run method

---
+++ src/Server.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server implements Runnable {
+
+    @Override
+    public void run() {
+        try (ServerSocket server = new ServerSocket(9999)) {
+            Socket client = server.accept();
+        } catch (IOException e) {
+            //TODO: handle
+        }
+    }
+
+    class ConnectionHandler implements Runnable {
+        private Socket client;
+        private BufferedReader in;
+        private PrintWriter out;
+
+        public ConnectionHandler(Socket client) {
+            this.client = client;
+        }
+
+        @Override
+        public void run() {
+            try {
+                out = new PrintWriter(client.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                out.println("Hello");
+            } catch (IOException e) {
+                //TODO: handle
+            }
+        }
+    }
+}
